d69cb22f0199112ad961f810dcfd896af697a935
zhs007 <sssxueren@gmail.com>
2023-09-21 16:03:55 +0800

    upd

--- src/nodes/cardgame3.js
+++ src/nodes/cardgame3.js
@@ -1,6 +1,74 @@
 (function (global) {
     var LiteGraph = global.LiteGraph;
 
+    function imgfuncOnAdded() {
+        if (this.properties["data"] != "" && this.img == null) {
+            this.loadImage(this.properties["data"]);
+        }
+    }
+
+    function imgfuncOnDrawBackground(ctx) {
+        if (this.flags.collapsed) {
+            return;
+        }
+        if (this.img && this.size[0] > 5 && this.size[1] > 5 && this.img.width) {
+            ctx.drawImage(this.img, 0, 0, this.size[0], this.size[1]);
+        }
+    }
+
+    function imgfuncOnPropertyChanged(name, value) {
+        this.properties[name] = value;
+        if (name == "data" && value != "") {
+            this.loadImage(value);
+        }
+
+        return true;
+    }
+
+    function imgfuncLoadImage(data, callback) {
+        if (data == "") {
+            this.img = null;
+            return;
+        }
+
+        this.img = document.createElement("img");
+
+        this.img.src = data;
+        this.boxcolor = "#F95";
+        var that = this;
+        this.img.onload = function () {
+            if (callback) {
+                callback(this);
+            }
+            console.log("Image loaded, size: " + that.img.width + "x" + that.img.height);
+            this.dirty = true;
+            that.boxcolor = "#9F9";
+            that.setDirtyCanvas(true);
+        };
+        this.img.onerror = function () {
+            console.log("error loading the image:" + data);
+        }
+    }
+
+    function imgfuncOnWidget(e, widget) {
+        if (widget.name == "load") {
+            this.loadImage(this.properties["data"]);
+        }
+    }
+
+    function imgfuncOnDropFile(file) {
+        var that = this;
+
+        const reader = new FileReader();
+        reader.onloadend = () => {
+            this.properties.data = reader.result;
+            this.loadImage(reader.result, function (img) {
+                that.size[1] = (img.height / img.width) * that.size[0];
+            });
+        };
+        reader.readAsDataURL(file);
+    };    
+
     function AttributeNode() {
         this.addInput("Change", "attribute");
         this.addInput("Check", "checker");
@@ -54,11 +122,11 @@
 
     GameStartNode.title = "GameStart";
 
-    LiteGraph.registerNodeType("cardgame3/gameStart", GameStartNode);    
+    LiteGraph.registerNodeType("cardgame3/gameStart", GameStartNode);
 
     function PersonNode() {
         this.addOutput("", "attribute");
-        this.properties = { url: "" };
+        this.properties = { data: "" };
         var that = this;
 
         this.text = this.addWidget("text", "Name", "name", function (v) { }, { textWidth: 0.7 });
@@ -68,82 +136,18 @@
     }
 
     PersonNode.title = "Person";
-    PersonNode.prototype.onAdded = function () {
-        if (this.properties["url"] != "" && this.img == null) {
-            this.loadImage(this.properties["url"]);
-        }
-    };
-
-    PersonNode.prototype.onDrawBackground = function (ctx) {
-        if (this.flags.collapsed) {
-            return;
-        }
-        if (this.img && this.size[0] > 5 && this.size[1] > 5 && this.img.width) {
-            ctx.drawImage(this.img, 0, 0, this.size[0], this.size[1]);
-        }
-    };
-
-    PersonNode.prototype.onPropertyChanged = function (name, value) {
-        this.properties[name] = value;
-        if (name == "url" && value != "") {
-            this.loadImage(value);
-        }
-
-        return true;
-    };
-
-    PersonNode.prototype.loadImage = function (url, callback) {
-        if (url == "") {
-            this.img = null;
-            return;
-        }
-
-        this.img = document.createElement("img");
-
-        if (url.substr(0, 4) == "http" && LiteGraph.proxy) {
-            url = LiteGraph.proxy + url.substr(url.indexOf(":") + 3);
-        }
-
-        this.img.src = url;
-        this.boxcolor = "#F95";
-        var that = this;
-        this.img.onload = function () {
-            if (callback) {
-                callback(this);
-            }
-            console.log("Image loaded, size: " + that.img.width + "x" + that.img.height);
-            this.dirty = true;
-            that.boxcolor = "#9F9";
-            that.setDirtyCanvas(true);
-        };
-        this.img.onerror = function () {
-            console.log("error loading the image:" + url);
-        }
-    };
-
-    PersonNode.prototype.onWidget = function (e, widget) {
-        if (widget.name == "load") {
-            this.loadImage(this.properties["url"]);
-        }
-    };
-
-    PersonNode.prototype.onDropFile = function (file) {
-        var that = this;
-        if (this._url) {
-            URL.revokeObjectURL(this._url);
-        }
-        this._url = URL.createObjectURL(file);
-        this.properties.url = this._url;
-        this.loadImage(this._url, function (img) {
-            that.size[1] = (img.height / img.width) * that.size[0];
-        });
-    };
+    PersonNode.prototype.onAdded = imgfuncOnAdded;
+    PersonNode.prototype.onDrawBackground = imgfuncOnDrawBackground;
+    PersonNode.prototype.onPropertyChanged = imgfuncOnPropertyChanged;
+    PersonNode.prototype.loadImage = imgfuncLoadImage;
+    PersonNode.prototype.onWidget = imgfuncOnWidget;
+    PersonNode.prototype.onDropFile = imgfuncOnDropFile;
 
     LiteGraph.registerNodeType("cardgame3/person", PersonNode);
 
     function EventNode() {
         this.addOutput("", "number");
-        this.properties = { url: "" };
+        this.properties = { data: "" };
         var that = this;
 
         this.text = this.addWidget("text", "Name", "name", function (v) { }, { textWidth: 0.7 });
@@ -153,76 +157,12 @@
     }
 
     EventNode.title = "Event";
-    EventNode.prototype.onAdded = function () {
-        if (this.properties["url"] != "" && this.img == null) {
-            this.loadImage(this.properties["url"]);
-        }
-    };
-
-    EventNode.prototype.onDrawBackground = function (ctx) {
-        if (this.flags.collapsed) {
-            return;
-        }
-        if (this.img && this.size[0] > 5 && this.size[1] > 5 && this.img.width) {
-            ctx.drawImage(this.img, 0, 0, this.size[0], this.size[1]);
-        }
-    };
-
-    EventNode.prototype.onPropertyChanged = function (name, value) {
-        this.properties[name] = value;
-        if (name == "url" && value != "") {
-            this.loadImage(value);
-        }
-
-        return true;
-    };
-
-    EventNode.prototype.loadImage = function (url, callback) {
-        if (url == "") {
-            this.img = null;
-            return;
-        }
-
-        this.img = document.createElement("img");
-
-        if (url.substr(0, 4) == "http" && LiteGraph.proxy) {
-            url = LiteGraph.proxy + url.substr(url.indexOf(":") + 3);
-        }
-
-        this.img.src = url;
-        this.boxcolor = "#F95";
-        var that = this;
-        this.img.onload = function () {
-            if (callback) {
-                callback(this);
-            }
-            console.log("Image loaded, size: " + that.img.width + "x" + that.img.height);
-            this.dirty = true;
-            that.boxcolor = "#9F9";
-            that.setDirtyCanvas(true);
-        };
-        this.img.onerror = function () {
-            console.log("error loading the image:" + url);
-        }
-    };
-
-    EventNode.prototype.onWidget = function (e, widget) {
-        if (widget.name == "load") {
-            this.loadImage(this.properties["url"]);
-        }
-    };
-
-    EventNode.prototype.onDropFile = function (file) {
-        var that = this;
-        if (this._url) {
-            URL.revokeObjectURL(this._url);
-        }
-        this._url = URL.createObjectURL(file);
-        this.properties.url = this._url;
-        this.loadImage(this._url, function (img) {
-            that.size[1] = (img.height / img.width) * that.size[0];
-        });
-    };
+    EventNode.prototype.onAdded = imgfuncOnAdded;
+    EventNode.prototype.onDrawBackground = imgfuncOnDrawBackground;
+    EventNode.prototype.onPropertyChanged = imgfuncOnPropertyChanged;
+    EventNode.prototype.loadImage = imgfuncLoadImage;
+    EventNode.prototype.onWidget = imgfuncOnWidget;
+    EventNode.prototype.onDropFile = imgfuncOnDropFile;
 
     LiteGraph.registerNodeType("cardgame3/event", EventNode);
 
